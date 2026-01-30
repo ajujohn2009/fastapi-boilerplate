6fa37616af6622878921ebfcc7e210141f66826d
Aju <aju@Ajus-MacBook-Pro.local>
2026-01-30 11:06:06 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI(title="Hello World")
+
+@app.get("/")
+def main():
+    return {"message": "Hello World latest code"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "OK"}
