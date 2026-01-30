79c829e9ab2b6c5ffc0a926e6a34fc1fc7a895bc
aju <aju@Ajus-MacBook-Pro.local>
2026-01-30 11:18:37 +0700

    ``` fix(main.py): update greeting message
    Updated the greeting message in main.py to reflect the new version
    naming, enhancing clarity for end-users and maintaining consistency
    with our versioning strategy.
    ```

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Hello World")
 
 @app.get("/")
 def main():
-    return {"message": "Hello World latest code"}
+    return {"message": "Hello World latest code version2"}
 
 
 @app.get("/health")
